f6d97a4b50413edd78cbf7d7d3c88ad5e08c79d7
Hui <jeffrey.x.hui@gmail.com>
2020-08-10 17:39:09 -0700

    implement word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,62 @@
-def word_count(s):
-    # Your code here
+#move through the string
+#notice when a word starts
 
+
+def word_count(s):
+
+    ignore = set()
+
+    ignore.add('"')
+    ignore.add(':')
+    ignore.add(';')
+    ignore.add(',')
+    ignore.add('.')
+    ignore.add('-')
+    ignore.add('+')
+    ignore.add('=')
+    ignore.add('/')
+    ignore.add("\r")
+    ignore.add("\n")
+    ignore.add("\t")
+    ignore.add("\\")
+    ignore.add('|')
+    ignore.add('[')
+    ignore.add(']')
+    ignore.add('{')
+    ignore.add('}')
+    ignore.add('(')
+    ignore.add(')')
+    ignore.add('*')
+    ignore.add('^')
+    ignore.add('&')
+
+    ignore.add(' ')
+
+    words = {}
+
+    word = ''
+    
+    for i in s:
+
+        if i in ignore:
+            if len(word): 
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+                word = ''
+
+        if i not in ignore:
+            
+            word += i.lower()
+
+    if len(word): 
+        if word in words:
+            words[word] += 1
+        else:
+            words[word] = 1
+
+    return words
 
 
 if __name__ == "__main__":
@@ -8,3 +64,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
