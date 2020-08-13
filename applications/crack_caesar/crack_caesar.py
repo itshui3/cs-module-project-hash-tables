0508bf0bf77c5eddba7b8ac3ef3677f14af006d5
Hui <jeffrey.x.hui@gmail.com>
2020-08-12 17:35:40 -0700

    built lookup table

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,12 @@
 
 # Your code here
 
+freq = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+cipherFreq = []
+
+# order letters from ciphertext.txt case non-sensitive by frequency
+# number of times letter occurs / total letters
+
+# loop through both freqs, aligning key: value as cipherFreq: freq
+# loop through ciphertext creating a new string with each cipherFreq letter replaced by freq
--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,15 +1,24 @@
 # Your code here
+import math
 
+cache = {}
+facCache = {}
 
-def slowfun_too_slow(x, y):
+def slowfun(x, y):
+    if (x, y) in cache: return cache[(x, y)]
+
     v = math.pow(x, y)
-    v = math.factorial(v)
+    if v in facCache: v = facCache[v]
+    else: 
+        facCache[v] = math.factorial(v)
+    v = facCache[v]
     v //= (x + y)
     v %= 982451653
 
+    cache[(x, y)] = v
     return v
 
-def slowfun(x, y):
+# def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
@@ -19,7 +28,7 @@ def slowfun(x, y):
 
 
 # Do not modify below this line!
-
+import random
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
